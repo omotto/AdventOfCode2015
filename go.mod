4d024e8677d1e33985616aad71b18c67451cfee8
Oscar Motto Varea <oscar.motto@mail.schwarz>
2025-01-15 11:34:58 +0100

    feat: day19 completed

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.23.3
 require (
 	github.com/dlclark/regexp2 v1.11.4
 	github.com/google/go-cmp v0.6.0
+	github.com/kkdai/cyk v0.0.0-20181107105257-d39a1d2186f4
 )
---
+++ src/day19/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"advent2015/pkg/file"
+	"fmt"
+	"math/rand"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+type Replacement struct {
+	a, b string
+}
+
+func parseInput(s []string) ([]Replacement, string) {
+	var (
+		a, b, str    string
+		change       = false
+		replacements []Replacement
+	)
+	for _, line := range s {
+		if len(line) < 2 {
+			change = true
+		} else {
+			if change == false {
+				_, _ = fmt.Sscanf(line, "%s => %s", &a, &b)
+				replacements = append(replacements, Replacement{a: a, b: b})
+			} else {
+				str = line
+			}
+		}
+	}
+	return replacements, str
+}
+
+func getNumMolecules(s []string) int {
+	replacements, str := parseInput(s)
+	molecules := map[string]struct{}{}
+	for _, replacement := range replacements {
+		nextIdx := 0
+		for {
+			if idx := strings.Index(str[nextIdx:], replacement.a); idx > -1 {
+				newStr := str[:idx+nextIdx] + strings.Replace(str[idx+nextIdx:], replacement.a, replacement.b, 1)
+				nextIdx += idx + len(replacement.a)
+				molecules[newStr] = struct{}{}
+				if nextIdx > len(str) {
+					break
+				}
+				continue
+			}
+			break
+		}
+	}
+	return len(molecules)
+}
+
+// Reverse algorithm, staring from final molecule to the initial "e"
+func getNumSteps(s []string) int {
+	var (
+		a, b, str    string
+		change       = false
+		replacements = map[string]string{}
+		keys         []string
+		deadEnd      bool
+	)
+	for _, line := range s {
+		if len(line) < 2 {
+			change = true
+		} else {
+			if change == false {
+				_, _ = fmt.Sscanf(line, "%s => %s", &a, &b)
+				replacements[b] = a
+			} else {
+				str = line
+			}
+		}
+	}
+	rand.Seed(time.Now().UnixNano())
+	for k, _ := range replacements {
+		keys = append(keys, k)
+	}
+	result := 0
+	molecule := str
+
+	for molecule != "e" {
+		deadEnd = true
+		for _, key := range keys {
+			// if replacement is not in the current molecule stage don't check it
+			if count := strings.Count(molecule, key); count > 0 {
+				result += count
+				molecule = strings.ReplaceAll(molecule, key, replacements[key])
+				deadEnd = false
+				break
+			}
+		}
+		// if deadEnd -> Try again
+		if deadEnd {
+			rand.Shuffle(len(keys), func(i, j int) {
+				keys[i], keys[j] = keys[j], keys[i]
+			})
+			molecule = str
+			result = 0
+		}
+	}
+	return result
+}
+
+func main() {
+	absPathName, _ := filepath.Abs("src/day19/input.txt")
+	output, _ := file.ReadInput(absPathName)
+
+	fmt.Println(getNumMolecules(output))
+	fmt.Println(getNumSteps(output))
+}
